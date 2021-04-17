64dc8ee5096c3605a3e65b9cf02988bbba43f883
Martin01987 <mplascencia@revartsstudio.com>
2021-04-17 14:07:58 -0500

    Primer commit, funcionamiento escena inicial y comienzo segunda escena.

---
+++ Assets/Scripts/GameMenuManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class GameMenuManager : MonoBehaviour
+{
+    [Header("Character Assets")]
+    public Animator characterAnimator;
+    public float rotationSpeed = 0.1f;
+    Transform characterTransform;
+    [Header("UI Assets")]
+    public CanvasGroup whiteFade;
+
+    void Awake(){
+        characterTransform = characterAnimator.transform;
+        characterAnimator.Play("Macarena_Dance");
+    }
+    public void ChangeCharacterAnimation(string animationName){
+        SetFade();
+        characterTransform.transform.rotation = Quaternion.Euler(0,180,0);
+    }
+
+    void SetFade(){
+        whiteFade.alpha = 1;
+        whiteFade.DOFade(0f,0.5f);
+    }
+
+    void Update(){
+        characterTransform.Rotate(0,rotationSpeed,0);
+    }
+}
---
+++ Assets/Scripts/StartMenuManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class StartMenuManager : MonoBehaviour
+{
+    [Header("Character Assets")]
+    public Animator characterAnimator;
+    public float rotationSpeed = 0.1f;
+    Transform characterTransform;
+    [Header("UI Assets")]
+    public CanvasGroup whiteFade;
+
+    void Awake(){
+        characterTransform = characterAnimator.transform;
+    }
+    public void ChangeCharacterAnimation(string animationName){
+        characterAnimator.Play(animationName);
+        SetFade();
+        characterTransform.transform.rotation = Quaternion.Euler(0,180,0);
+    }
+
+    public void GoToGame(){
+        
+    }
+
+    void SetFade(){
+        whiteFade.alpha = 1;
+        whiteFade.DOFade(0f,0.5f);
+    }
+
+    void Update(){
+        characterTransform.Rotate(0,rotationSpeed,0);
+    }
+}
